6e23da283bb6168192f495077af9f131bff2a484
sueraychang <sueraychang@gmail.com>
2021-01-05 18:05:31 +0800

    Implement GesturedImageView.

--- library/src/main/java/com/src/gesturedimageview/GesturedImageView.kt
+++ library/src/main/java/com/src/gesturedimageview/GesturedImageView.kt
@@ -1,7 +1,12 @@
 package com.src.gesturedimageview
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.Matrix
+import android.graphics.PointF
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.util.AttributeSet
 import android.util.Log
 import android.view.GestureDetector
@@ -9,17 +14,20 @@ import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import android.view.View.OnTouchListener
 import androidx.appcompat.widget.AppCompatImageView
+import java.lang.Float.max
+import java.lang.Float.min
+import java.lang.Integer.min
 
 class GesturedImageView : AppCompatImageView {
 
-    interface PanToEndListener {
+    interface ScrollToEndListener {
 
-        fun onPanToHorizontalEnd(toEnd: Boolean)
+        fun onScrollToHorizontalEnd(direction: Int)
 
-        fun onPanToVerticalEnd(toEnd: Boolean)
+        fun onScrollToVerticalEnd(direction: Int)
     }
 
-    private var listener: PanToEndListener? = null
+    private var listener: ScrollToEndListener? = null
 
     constructor(context: Context) : super(context) {
         initialView(context)
@@ -30,61 +38,54 @@ class GesturedImageView : AppCompatImageView {
         initialAttrs(attributeSet)
     }
 
+    constructor(context: Context, attributeSet: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attributeSet,
+        defStyleAttr
+    ) {
+        initialView(context)
+        initialAttrs(attributeSet)
+        initialStyle(defStyleAttr)
+    }
+
     private lateinit var scaleGestureDetector: ScaleGestureDetector
     private lateinit var gestureDetector: GestureDetector
 
-    private var lastX = 0f
-    private var lastY = 0f
+    private var viewWidth = 0
+    private var viewHeight = 0
 
-    private var savedScale = 1f
+    private var gesturedScaleType = ScaleType.FIT_CENTER
+
+    private var lastTouch = PointF(0f, 0f)
+
+    private var currentScale = 1f
 
     private val matrixValues = FloatArray(9)
     private var displayMatrix = Matrix()
 
     private var activePointerId = -1
 
-    private var isPanToHorizontalEnd: Boolean = false
-    private var isPanToVerticalEnd: Boolean = false
+    private var isScrolledToHorizontalEnd: Int = 0
+    private var isScrolledToVerticalEnd: Int = 0
 
     private val scaleGestureListener =
         object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
             override fun onScaleBegin(detector: ScaleGestureDetector?): Boolean {
-//                Log.d(TAG, "onScaleBegin")
                 return true
             }
 
             override fun onScale(detector: ScaleGestureDetector): Boolean {
-                Log.d(TAG, "onScale")
-                val prevScale = savedScale
-                var scaleFactor = detector.scaleFactor
-                savedScale *= detector.scaleFactor
-                if (savedScale > MAX_SCALE) {
-                    savedScale = MAX_SCALE
-                    scaleFactor = MAX_SCALE / prevScale
-                }
-                if (savedScale < MIN_SCALE) {
-                    savedScale = MIN_SCALE
-                    scaleFactor = MIN_SCALE / prevScale
-                }
-                displayMatrix.postScale(
-                    scaleFactor,
-                    scaleFactor,
-                    detector.focusX,
-                    detector.focusY
-                )
-                imageMatrix = displayMatrix
-                fixTranslation()
+                scaleImage(detector.scaleFactor, detector.focusX, detector.focusY)
                 return true
             }
 
             override fun onScaleEnd(detector: ScaleGestureDetector?) {
-//                Log.d(TAG, "onScaleEnd")
             }
         }
     private val gestureListener = object : GestureDetector.SimpleOnGestureListener() {
         override fun onDoubleTap(e: MotionEvent): Boolean {
             Log.d(TAG, "onDoubleTap")
-            fitToScreen()
+//            fitToScreen()
             return true
         }
     }
@@ -97,24 +98,16 @@ class GesturedImageView : AppCompatImageView {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
                 event.actionIndex.let {
-                    lastX = event.getX(it)
-                    lastY = event.getY(it)
+                    lastTouch.set(event.getX(it), event.getY(it))
                 }
-//                state = State.DRAG
                 activePointerId = event.getPointerId(0)
             }
             MotionEvent.ACTION_MOVE -> {
                 val (x: Float, y: Float) = event.findPointerIndex(activePointerId).let {
                     event.getX(it) to event.getY(it)
                 }
-                val dx = x - lastX
-                val dy = y - lastY
-                displayMatrix.postTranslate(dx, dy)
-                imageMatrix = displayMatrix
-                fixTranslation()
-
-                lastX = x
-                lastY = y
+                translateImage(x - lastTouch.x, y - lastTouch.y)
+                lastTouch.set(x, y)
             }
             MotionEvent.ACTION_POINTER_UP -> {
                 event.actionIndex.also { pointerIndex ->
@@ -122,8 +115,7 @@ class GesturedImageView : AppCompatImageView {
                         .takeIf { it == activePointerId }
                         ?.run {
                             val newPointerIndex = if (pointerIndex == 0) 1 else 0
-                            lastX = event.getX(newPointerIndex)
-                            lastY = event.getY(newPointerIndex)
+                            lastTouch.set(event.getX(newPointerIndex), event.getY(newPointerIndex))
                             activePointerId = event.getPointerId(newPointerIndex)
                         }
                 }
@@ -136,8 +128,94 @@ class GesturedImageView : AppCompatImageView {
         true
     }
 
-    fun setOnPanToEndListener(listener: PanToEndListener) {
+    var maxScale: Float = 1f
+    var minScale: Float = 1f
+
+    fun setScrollToEndListener(listener: ScrollToEndListener) {
         this.listener = listener
+    }
+
+    override fun setScaleType(scaleType: ScaleType) {
+        Log.d(TAG, "setScaleType $scaleType")
+        if (scaleType == ScaleType.MATRIX) {
+            super.setScaleType(scaleType)
+        } else {
+            gesturedScaleType = scaleType
+        }
+    }
+
+    override fun getScaleType() = gesturedScaleType
+
+    override fun setImageResource(resId: Int) {
+        Log.d(TAG, "setImageResource $resId")
+        super.setImageResource(resId)
+        initialImage()
+    }
+
+    override fun setImageBitmap(bm: Bitmap) {
+        Log.d(TAG, "setImageBitmap $bm")
+        super.setImageBitmap(bm)
+        initialImage()
+    }
+
+    override fun setImageDrawable(drawable: Drawable?) {
+        Log.d(TAG, "setImageDrawable $drawable")
+        super.setImageDrawable(drawable)
+        initialImage()
+    }
+
+    override fun setImageURI(uri: Uri?) {
+        Log.d(TAG, "setImageURI $uri")
+        super.setImageURI(uri)
+        initialImage()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        Log.d(TAG, "onMeasure $drawable ${drawable?.intrinsicWidth} ${drawable?.intrinsicHeight}")
+        if (drawable == null || drawable.intrinsicWidth == 0 || drawable.intrinsicWidth == 0) {
+            setMeasuredDimension(0, 0)
+            return
+        }
+
+        val widthSize = MeasureSpec.getSize(widthMeasureSpec)
+        val widthMode = MeasureSpec.getMode(widthMeasureSpec)
+        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
+        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+        val totalWidth = getViewSize(widthMode, widthSize, drawable.intrinsicWidth)
+        val totalHeight = getViewSize(heightMode, heightSize, drawable.intrinsicHeight)
+
+        setMeasuredDimension(
+            totalWidth - paddingLeft - paddingRight,
+            totalHeight - paddingTop - paddingBottom
+        )
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        Log.d(TAG, "onLayout $changed $left $top $right $bottom")
+        super.onLayout(changed, left, top, right, bottom)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        Log.d(TAG, "onDraw")
+        super.onDraw(canvas)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        Log.d(TAG, "onSizeChanged $w $h $oldw $oldh")
+
+        viewWidth = w
+        viewHeight = h
+
+        initialImage()
+    }
+
+    override fun canScrollHorizontally(direction: Int): Boolean {
+        return isScrolledToHorizontalEnd == 0
+    }
+
+    override fun canScrollVertically(direction: Int): Boolean {
+        return isScrolledToVerticalEnd == 0
     }
 
     private fun initialView(context: Context) {
@@ -150,22 +228,40 @@ class GesturedImageView : AppCompatImageView {
         scaleGestureDetector = ScaleGestureDetector(context, scaleGestureListener)
         gestureDetector = GestureDetector(context, gestureListener)
         setOnTouchListener(touchListener)
-
     }
 
     private fun initialAttrs(attributeSet: AttributeSet) {
 
     }
 
-    private fun fitToScreen() {
-        val scaleFactor = 1 / savedScale
-        displayMatrix.postScale(scaleFactor, scaleFactor)
-        imageMatrix = displayMatrix
+    private fun initialStyle(defStyleAttr: Int) {
 
-        savedScale = 1f
     }
 
-    private fun fixTranslation() {
+    private fun scaleImage(scaleFactor: Float, focusX: Float, focusY: Float) {
+        val prevScale = currentScale
+        currentScale *= scaleFactor
+        var realScaleFactor = scaleFactor
+        if (currentScale > maxScale) {
+            currentScale = maxScale
+            realScaleFactor = maxScale / prevScale
+        }
+        if (currentScale < minScale) {
+            currentScale = minScale
+            realScaleFactor = minScale / prevScale
+        }
+        displayMatrix.postScale(realScaleFactor, realScaleFactor, focusX, focusY)
+        imageMatrix = displayMatrix
+//        fixTranslation()
+    }
+
+    private fun translateImage(dx: Float, dy: Float) {
+        displayMatrix.postTranslate(dx, dy)
+        imageMatrix = displayMatrix
+        correctBoundary()
+    }
+
+    private fun correctBoundary() {
         imageMatrix.getValues(matrixValues)
         matrixValues.apply {
             val transX = this[Matrix.MTRANS_X]
@@ -173,33 +269,37 @@ class GesturedImageView : AppCompatImageView {
 
             Log.d(TAG, "transX $transX transY $transY")
 
-            val fixTransX = getFixTranslation(transX, width.toFloat(), width * savedScale)
-            val fixTransY = getFixTranslation(transY, height.toFloat(), height * savedScale)
-            Log.d(TAG, "fixTransX $fixTransX fixTransY $fixTransY")
+            val correctionX =
+                getCorrection(transX, viewWidth.toFloat(), drawable.intrinsicWidth * currentScale)
+            val correctionY =
+                getCorrection(transY, viewHeight.toFloat(), drawable.intrinsicHeight * currentScale)
+            Log.d(TAG, "correctionX $correctionX correctionY $correctionY")
 
-            if (fixTransX != 0f || fixTransY != 0f) {
-                displayMatrix.postTranslate(fixTransX, fixTransY)
+            if (correctionX != 0f || correctionY != 0f) {
+                displayMatrix.postTranslate(correctionX, correctionY)
                 imageMatrix = displayMatrix
             }
 
-            if (fixTransX != 0f && !isPanToHorizontalEnd) {
-                isPanToHorizontalEnd = true
-                listener?.onPanToHorizontalEnd(true)
-            } else if (fixTransX == 0f && isPanToHorizontalEnd) {
-                isPanToHorizontalEnd = false
-                listener?.onPanToHorizontalEnd(false)
+            Log.d(TAG, "$isScrolledToHorizontalEnd $isScrolledToVerticalEnd")
+            if (correctionX != 0f && (correctionX * isScrolledToHorizontalEnd) == 0f) {
+                isScrolledToHorizontalEnd = if (correctionX > 0) 1 else -1
+                listener?.onScrollToHorizontalEnd(isScrolledToHorizontalEnd)
+            } else if (correctionX == 0f && isScrolledToHorizontalEnd != 0) {
+                isScrolledToHorizontalEnd = 0
+                listener?.onScrollToHorizontalEnd(0)
             }
 
-            if (fixTransY != 0f && !isPanToVerticalEnd) {
-                isPanToVerticalEnd = true
-                listener?.onPanToVerticalEnd(true)
-            } else if (fixTransY == 0f && isPanToVerticalEnd) {
-                listener?.onPanToVerticalEnd(false)
+            if (correctionY != 0f && (correctionY * isScrolledToVerticalEnd) == 0f) {
+                isScrolledToVerticalEnd = if (correctionY > 0) -1 else 1
+                listener?.onScrollToVerticalEnd(isScrolledToVerticalEnd)
+            } else if (correctionY == 0f && isScrolledToVerticalEnd != 0) {
+                isScrolledToVerticalEnd = 0
+                listener?.onScrollToVerticalEnd(isScrolledToVerticalEnd)
             }
         }
     }
 
-    private fun getFixTranslation(trans: Float, viewSize: Float, contentSize: Float): Float {
+    private fun getCorrection(trans: Float, viewSize: Float, contentSize: Float): Float {
         val minTrans: Float
         val maxTrans: Float
         if (contentSize <= viewSize) {
@@ -220,16 +320,58 @@ class GesturedImageView : AppCompatImageView {
         return 0f
     }
 
-//    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-//        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-//        Log.d(TAG, "onMeasure width $width height $height")
-//    }
+    private fun initialImage() {
+        Log.d(TAG, "transformImage")
+        if (drawable == null || drawable.intrinsicWidth == 0 || drawable.intrinsicHeight == 0) {
+            return
+        }
+
+        var scaleWidth = viewWidth / drawable.intrinsicWidth.toFloat()
+        var scaleHeight = viewHeight / drawable.intrinsicHeight.toFloat()
+        Log.d(TAG, "$scaleWidth $scaleHeight")
+        Log.d(TAG, "gesturedScaleType $gesturedScaleType")
+        when (gesturedScaleType) {
+            ScaleType.FIT_CENTER -> {
+                min(scaleWidth, scaleHeight).apply {
+                    scaleWidth = this
+                    scaleHeight = this
+                }
+                displayMatrix.postScale(scaleWidth, scaleHeight)
+            }
+            ScaleType.CENTER_CROP -> {
+                max(scaleWidth, scaleHeight).apply {
+                    scaleWidth = this
+                    scaleHeight = this
+                }
+                displayMatrix.postScale(scaleWidth, scaleHeight)
+            }
+            else -> {
+
+            }
+        }
+
+        val diffWidth = viewWidth - scaleWidth * drawable.intrinsicWidth
+        val diffHeight = viewHeight - scaleHeight * drawable.intrinsicHeight
+
+        when (gesturedScaleType) {
+            ScaleType.FIT_CENTER -> {
+                displayMatrix.postTranslate(diffWidth / 2, diffHeight / 2)
+            }
+        }
+
+        imageMatrix = displayMatrix
+    }
+
+    private fun getViewSize(mode: Int, size: Int, drawableSize: Int): Int {
+        return when (mode) {
+            MeasureSpec.EXACTLY -> size
+            MeasureSpec.AT_MOST -> min(drawableSize, size)
+            MeasureSpec.UNSPECIFIED -> drawableSize
+            else -> size
+        }
+    }
 
     companion object {
-
-        private const val TAG = "[IG]GesturalImageView"
-
-        private const val MAX_SCALE = 4f
-        private const val MIN_SCALE = 1f
+        private const val TAG = "[GesturedImageView]"
     }
 }
--- sample/src/main/java/com/src/gesturedimageview/sample/MainActivity.kt
+++ sample/src/main/java/com/src/gesturedimageview/sample/MainActivity.kt
@@ -2,10 +2,37 @@ package com.src.gesturedimageview.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.bumptech.glide.Glide
+import com.src.gesturedimageview.GesturedImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gesturedImageView = findViewById<GesturedImageView>(R.id.gestured_image_view).apply {
+            maxScale = 4f
+            minScale = 1f
+
+            setScrollToEndListener(object: GesturedImageView.ScrollToEndListener{
+                override fun onScrollToHorizontalEnd(direction: Int) {
+                    Log.d(TAG, "onScrollToHorizontalEnd $direction")
+                }
+
+                override fun onScrollToVerticalEnd(direction: Int) {
+                    Log.d(TAG, "onScrollToVerticalEnd $direction")
+                }
+            })
+        }
+
+        Glide.with(this)
+            .load("https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/1280px-Test-Logo.svg.png")
+            .fitCenter()
+            .into(gesturedImageView)
+    }
+
+    companion object {
+        private const val TAG = "[GesturedImageView]"
     }
 }
